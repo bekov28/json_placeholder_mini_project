5390396fd05cb422c19b269c0aaf4aabd65ba72d
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-05-30 00:28:19 +0900

    fetched data

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 window.addEventListener("DOMContentLoaded", () => {
-  fetch("https://jsonplaceholder.typicode1.com/posts", {
+  const postsWrapper = document.querySelector(".posts");
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   })
-    .then((response) => response.json()) //convertinf to JSON
+    .then((response) => response.json()) //Parses JSON from the response to object
     .then((data) => {
-      console.log(data);
+      data.forEach((item, index) => {
+        const post = document.createElement("div");
+        post.classList.add("post");
+        post.innerHTML += `
+        <h4><b>#${index + 1}.</b>${item.title}</h4>
+          <p>${item.body}</p>
+        `;
+        postsWrapper.append(post);
+      });
     })
     .catch((err) => {
       console.log("ERROR", err);
     })
-    .finally(() => console.log('FINALLY'))
+    .finally(() => console.log("FINALLY"));
 });
